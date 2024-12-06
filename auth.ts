4026e45184a0d8d258f7445e33cd31a55730d284
semih babacan huawei <m.semihbabacan@gmail.com>
2024-12-06 18:26:39 +0300

    feat: added hashing in auth

--- auth.ts
+++ auth.ts
@@ -2,25 +2,25 @@ import NextAuth from 'next-auth';
 import authConfig from './auth.config';
 import { database } from '@/lib/firebase/database';
 import { CurrentUser } from './types/models/user';
+import { createHash } from 'crypto';
 
 export const { auth, handlers, signOut, signIn } = NextAuth({
   ...authConfig,
   events: {
     async signIn({ user }) {
       try {
-        // Check if user has an ID
-        if (!user.id) {
-          console.error('No user ID found');
-          return;
-        }
-        console.log('User ID:', user.id);
-        // Check if user exists in Firebase
-        const existingUser = await database.getOne('users', user.id);
+        // using the user email and name, generate a unique id,
+        // use a hashing function to generate the id
+        const id = createHash('sha256')
+          .update(user.email! + user.name!)
+          .digest('hex');
+
+        const existingUser = await database.getOne('users', id);
 
         if (!existingUser) {
           // Create new user if doesn't exist
           const newUser: CurrentUser = {
-            id: user.id,
+            id: id,
             name: user.name,
             email: user.email,
             image: user.image,
