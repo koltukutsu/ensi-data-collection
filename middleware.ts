361bc581be58a173251e71c600c723aaeb7c0c30
semih babacan huawei <m.semihbabacan@gmail.com>
2024-12-12 12:40:11 +0300

    feat: update

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,21 @@
-// Protecting routes with next-auth
-// https://next-auth.js.org/configuration/nextjs#middleware
-// https://nextjs.org/docs/app/building-your-application/routing/middleware
+// // Protecting routes with next-auth
+// // https://next-auth.js.org/configuration/nextjs#middleware
+// // https://nextjs.org/docs/app/building-your-application/routing/middleware
 
-import NextAuth from 'next-auth';
-import authConfig from './auth.config';
+// import NextAuth from 'next-auth';
+// import authConfig from './auth.config';
 
-export const { auth } = NextAuth(authConfig);
+// export const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const isOnDashboard = req.nextUrl.pathname.startsWith('/dashboard');
+// export default auth((req) => {
+//   const isLoggedIn = !!req.auth;
+//   const isOnDashboard = req.nextUrl.pathname.startsWith('/dashboard');
 
-  if (isOnDashboard && !isLoggedIn) {
-    return Response.redirect(new URL('/', req.url));
-  }
+//   if (isOnDashboard && !isLoggedIn) {
+//     return Response.redirect(new URL('/', req.url));
+//   }
 
-  return null;
-});
+//   return null;
+// });
 
-export const config = { matcher: ['/dashboard/:path*'] };
+// export const config = { matcher: ['/dashboard/:path*'] };
